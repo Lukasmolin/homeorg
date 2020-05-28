93d740950dcb18b1fcf07b914dc183f8500ab7c8
Lucas <lukasmolin@gmail.com>
2020-05-28 15:10:14 -0300

    Creates Json Api processor

---
+++ node-api/src/json/jsonApiProcessor.js
@@ -0,0 +1,19 @@
+/**
+ * 
+ * @param {Object} toSend valid JSON or instanceOf Error Object to send
+ * @param {Boolean} [error = false] if the request had error, true if toSend is instanceOf Error, defaults to false otherwise
+ * @returns {JSON} JSON formatted to comply with JSON API
+ * @see {@link "https://jsonapi.org/format/"} for more info
+ */
+function format(toSend, error = false){
+    const resp = {};
+    if(toSend instanceof Error)
+        resp.error = toSend.message;
+    else if(error)
+        resp.error = toSend;
+    else
+        resp.data = toSend;
+
+    return resp;
+}
+module.exports = format;
