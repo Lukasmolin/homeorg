45d3df9346f2602ab9a0494e68a49cf73ac22c73
Lucas <lukasmolin@gmail.com>
2020-05-29 20:26:10 -0300

    partial implementation and relocation of utils

--- node-api/src/controller/userController.js
+++ node-api/src/controller/userController.js
@@ -1,6 +1,17 @@
 const UserService = require('../service/userService');
-const jsonfy = require('../json/jsonApiProcessor');
+const jsonfy = require('../util/json/jsonApiProcessor');
+const BadRequestError = require('../error/BadRequestError');
+const { User } = require('../model');
+const { isString } = require('../util/typeChecks/typeCheck');
+
 const userService = new UserService();
+
+function handleError(error, res){
+    console.error(error);
+    const json = jsonfy(error);
+    const status = error.status || 500;
+    res.status(status).json(json);
+}
 
 module.exports = {
     async index(req, res){
@@ -9,21 +20,65 @@ module.exports = {
             const json = jsonfy(resp);
             return res.json(json);
         } catch (error) {
-            const json = jsonfy(error);
-            const status = error.status || 500;
-            return res.status(status).json(json);
+            return handleError(error, res);
         }
     },
     async info(req, res){
-        return res.status(501).json({});
+        const { id } = req.params;
+        try {
+            const resp = await userService.get(id);
+            const json = jsonfy(resp);
+            return res.json(json);
+        } catch (error) {
+            return handleError(error, res);
+        }
     },
     async create(req, res){
-        return res.status(501).json({});
+        if(!req.body.data)
+            return handleError(new BadRequestError('No data prop inside payload'), res);
+
+        const { username, password } = req.body.data;
+        if(!username || !password || !isString(username) || !isString(username))
+            return handleError(new BadRequestError('Data must contain valid string username and password'), res);
+
+        try {
+            
+            const resp = await userService.create(username, password);
+            const json = jsonfy(resp);
+            return res.json(json);
+        } catch (error) {
+            return handleError(error, res);
+        }
     },
     async edit(req, res){
-        return res.status(501).json({});
+        const { id } = req.params;
+        if(isNaN(id) || id <= 0)
+            return handleError(new BadRequestError('Invalid Id'), res);
+
+        if(!req.body.data)
+            return handleError(new BadRequestError('No data prop inside payload'), res);
+
+        const { username, password } = req.body.data;
+        const editUser = new User(username, password, id);
+        try {
+            const resp = await userService.edit(editUser);
+            const json = jsonfy(resp);
+            return res.json(json);
+        } catch (error) {
+            return handleError(error, res);
+        }
     },
     async delete(req, res){
-        return res.status(501).json({});
+        const { id } = req.params;
+        if(isNaN(id) || id <= 0)
+            return handleError(new BadRequestError('Invalid id'), res);
+
+        try {
+            const resp = await userService.delete(id);
+            const json = jsonfy(resp);
+            return res.json(json);
+        } catch (error) {
+            return handleError(error, res);
+        }        
     }
 }
--- node-api/src/service/userService.js
+++ node-api/src/service/userService.js
@@ -1,4 +1,6 @@
 const UserRepository = require('../repository/userRepository');
+const InvalidParamError = require('../error/invalidParamError');
+const { User } = require('../model');
 
 module.exports = class UserService {
 
@@ -6,41 +8,60 @@ module.exports = class UserService {
         this.repo = new UserRepository();
     }
 
+    /**
+     * Gets all users with id and username populated
+     * @returns {Promise<User[]>} all users
+     */
     async getAll(){
         const resp = await this.repo.selectAll();
         return resp;
     }
     
-    async create(newUser){
-        try {
-            
-        } catch (error) {
+    /**
+     * Creates new user
+     * @param {User|String} newUser string with username is expected if second parameter is passed, otherwise {User}
+     * with username and password expected
+     * @param {String} [password] user password
+     */
+    async create(newUser, password){
+        let user;
+        if(typeof newUser === 'string' || newUser instanceof String){
             
         }
+
+        if(!(newUser instanceof User)) throw new InvalidParamError("Param: NewUser Must be instanceof User");
+
+        const resp = await this.repo.insert(newUser);
+        return resp;
     }
 
+    /**
+     * @param {(Number|User|String)} id Either a instanceOf User with a valid id prop or the id itself
+     * @returns {Promise<User>} selected user
+     * @throws {InvalidParamError} if negative number or invalid {id} type 
+     */
     async get(id){
-        try {
-            
-        } catch (error) {
-            
-        }
+        if(isNaN(id) && !(id instanceof User))
+            throw new InvalidParamError('Param: id is neither a number nor instanceof User.');
+
+        if(id <= 0)
+            throw new InvalidParamError('Param: id must be a positive number');
+
+        const userId = isNaN(id) ? user.id : parseInt(id);
+        const resp = await this.repo.select(userId);
+        return resp;
     }
 
-    async edit(id, user){
-        try {
-            
-        } catch (error) {
-            
-        }
+    async edit(user){
+        if(!(user instanceof User)) throw new InvalidParamError('')
+
+        const { id } = user;
+        if(isNaN(id) || id <= 0) throw new InvalidParamError('')
+        
     }
 
     async delete(id){
-        try {
-            
-        } catch (error) {
-            
-        }
+        
     }
 
 }
