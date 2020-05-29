bbcb30b0c58bc0ae045d6a9c0b5cd3fdc8eb11f4
Lucas <lukasmolin@gmail.com>
2020-05-29 01:14:49 -0300

    Failsafes and documentation

--- node-api/src/repository/userRepository.js
+++ node-api/src/repository/userRepository.js
@@ -10,37 +10,44 @@ module.exports = class UserRepository {
     }
 
     async insert(user){
-        if(!(user instanceof User)){
-            const err = new InvalidParamError('Param must be instanceof User');
-            console.error(err);
-            throw err;
-        }
+        if(!(user instanceof User))
+            throw new InvalidParamError('Param must be instanceof User');
+
         try {
             const inserted = await this.getTable().insert(user);
             return inserted;
         } catch (error) {
-            console.error(error);
-            return new DatabaseError(error.message);
-        }
-    }
-
-    async deleteById(id){
-        if(isNaN(id)) throw new InvalidParamError("Id is Not a Number");
-        try {
-            const deleted = await this.getTable().where('id', id).delete().returning('*').first();
-            return new User(deleted);
-        } catch (error) {
-            console.error(error);
             throw new DatabaseError(error.message);
         }
     }
 
-    async update(user){
-        if(!(user instanceof User)){
-            const err = new InvalidParamError('Param must be instanceof User');
-            console.error(err);
-            throw err;
+    /**
+     * @param {Number} id positive id number
+     */
+    async deleteById(id){
+        if(isNaN(id)) throw new InvalidParamError('Id is Not a Number');
+        if(id <= 0) throw new InvalidParamError('Id is not a valid number');
+
+        const userId = parseInt(id);
+        try {
+            const deleted = await this.getTable().where('id', userId).delete().returning('*').first();
+            return new User(deleted);
+        } catch (error) {
+            throw new DatabaseError(error.message);
         }
+    }
+
+    /**
+     * @param {User} user with a valid id
+     * @returns {Promise} 
+     */
+    async update(user){
+        if(!(user instanceof User))
+            throw new InvalidParamError('Param must be instanceof User');
+
+        if(isNaN(user.id) || user.id <= 0)
+            throw new InvalidParamError('user.id must be a valid number!')
+
         try {
             const user = await this.getTable()
                 .where('id', user.id)
@@ -50,27 +57,31 @@ module.exports = class UserRepository {
                  });
             return user;
         } catch (error) {
-            console.error(error);
             throw new DatabaseError(error.message);
         }
     }
 
-    async select(user){
+    /**
+     * @param {Number} id of the user to get
+     * @returns {Promise<User>} user with the specified id
+     */
+    async select(id){
         try {
-            const [user] = await this.getTable().where('id', user.id);
+            const [user] = await this.getTable().where('id', id);
             return user;
         } catch (error) {
-            console.error(error);
             throw new DatabaseError(error.message);
         }
     }
 
+    /**
+     * @returns {Promise<User[]>} All users with ids and names populated
+     */
     async selectAll(){
         try {
             const users = await this.getTable().select('id', 'username');
             return users.map(user => new User(user));
         } catch (error) {
-            console.error(error);
             throw new DatabaseError(error.message);
         }
     }
